980b6139fc5dbc218665021740f5aec279b47794
sum256 <sumuruzuna0514@gmail.com>
2024-12-09 15:51:37 +0900

    headerにCSSを適用。その他ちょいちょい

--- websites/skill.php
+++ websites/skill.php
@@ -6,6 +6,25 @@
     <title>Document</title>
 </head>
 <body>
-    
+<header>
+        <ul>
+            <li>
+                <a href="index.php">Home</a>
+            </li>
+            <li>
+                <a href="index.php#contests">Contests</a>
+            </li>
+            <li>
+                <a href="/skill">SKILL</a>
+            </li>
+            <li>
+                <a href="/results">Results</a>
+            </li>
+            <li>
+                <a href="#Contact">Contact</a>
+            </li>
+        </ul>
+    </header>
+    <div></div>
 </body>
 </html>
